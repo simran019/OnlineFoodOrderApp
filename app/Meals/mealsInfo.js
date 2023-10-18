f3cd10c2d2947d16d85b186f4b8fd8127dab991b
simran019 <simran.19.1677@gmail.com>
2023-10-18 13:19:44 +0530

    show meals

---
+++ app/Meals/mealsInfo.js
@@ -0,0 +1,28 @@
+const allMeals = [
+ {
+    mealId:"meal1",
+    mealName:"Sushi",
+    mealDescription:"Fine fish with Veggies",
+    mealAmount:"Rs550"
+ },
+ {
+    mealId:"meal2",
+    mealName:"Burger",
+    mealDescription:"Healthy and green",
+    mealAmount:"Rs250"
+ },
+ {
+    mealId:"meal3",
+    mealName:"Soup",
+    mealDescription:"All green veggies",
+    mealAmount:"Rs350"
+ },
+ {
+    mealId:"meal4",
+    mealName:"Ice Cream",
+    mealDescription:"Blueberries and nuts",
+    mealAmount:"Rs150"
+ }    
+]
+
+export default allMeals;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Header from "./Header/page";
 import PictureSection from "./PictureSection/page";
+import Meals from "./Meals/page";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <Header/>
       <PictureSection/>
+      <Meals/>
     </main>
   );
 }
